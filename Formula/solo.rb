1786bea16c0d8a3f9a65a62fc8a6746b22afae2d
Jeffrey Tang <jeffrey@swirldslabs.com>
2025-10-28 14:51:37 -0500

    create separate file
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- Formula/solo.rb
+++ Formula/solo.rb
@@ -4,21 +4,12 @@ class Solo < Formula
 
   url "https://registry.npmjs.org/@hashgraph/solo/-/solo-0.48.0.tgz"
   sha256 "c6bb42b303e8785940c8870415350d42b02ea4e1c7e7151e82d530a1d5657b75"
-  version "0.48.0" # Explicitly define the version
-
-  resource "0.47.0" do
-    url "https://registry.npmjs.org/@hashgraph/solo/-/solo-0.47.0.tgz"
-    sha256 "9b640c698942a569be083cff82eb7a503ad46a430faa65d7f3ab229b124a92f1"
-  end
+  version "0.48.0"
 
   depends_on "node"
 
   def install
-    # This runs `npm install` inside a sandbox with Homebrew's
-    # recommended settings for global packages.
     system "npm", "install", *std_npm_args
-
-    # npm installs the executable to libexec/bin. You need to symlink it to bin.
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
