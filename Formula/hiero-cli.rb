fe87bca2fa8eea7ed456e9a25e8c3f242f8fb8ff
Michiel Mulders <michielmulders1@gmail.com>
2026-02-02 15:58:48 +0100

    Add hiero-cli formula
    Signed-off-by: Michiel Mulders <michielmulders1@gmail.com>

---
+++ Formula/hiero-cli.rb
@@ -0,0 +1,25 @@
+require "language/node"
+class HieroCli < Formula
+  desc "Command-line interface for interacting with the Hedera network"
+  homepage "https://github.com/hiero-ledger/hiero-cli"
+  url "https://registry.npmjs.org/@hiero-ledger/hiero-cli/-/hiero-cli-0.11.1.tgz"
+  sha256 "49ab436e5c79533c31c6b9072325779470cc0615832e81ffc3dac5ef900032d0"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  def caveats
+    <<~EOS
+      First-time setup (Initialization): When you run any command that requires an operator (like transferring HBAR or creating fungible tokens) in interactive mode, the CLI will automatically launch an initialization wizard to guide you through configuring the operator account, private key, and settings. In script mode (non-interactive), an error will be thrown instead, requiring you to use hcli network set-operator to configure the operator first.
+    EOS
+  end
+
+  test do
+    system "#{bin}/hcli", "--help"
+  end
+end
