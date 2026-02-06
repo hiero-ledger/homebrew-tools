6372cd425b4bca2691642e36bfda0690e24d8798
Jeffrey Tang <jeffrey@swirldslabs.com>
2026-02-06 17:22:12 -0600

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- Formula/solo.rb
+++ Formula/solo.rb
@@ -9,6 +9,48 @@ class Solo < Formula
   depends_on "node"
 
   def install
+    npm_packages = ["@hiero-ledger/solo", "@hashgraph/solo"]
+    npm_root = Utils.popen_read("npm", "root", "-g").strip
+
+    npm_packages.each do |pkg|
+      pkg_scope, pkg_name = pkg.split("/")
+      pkg_path = File.join(npm_root, pkg_scope, pkg_name)
+
+      if !npm_root.empty? && File.symlink?(pkg_path)
+        opoo <<~EOS
+          ATTENTION: Detected a global npm link for #{pkg}.
+          Removing it to avoid conflicts with the Homebrew install.
+        EOS
+        system "npm", "unlink", "-g", pkg
+
+        if File.symlink?(pkg_path)
+          opoo <<~EOS
+            ATTENTION: npm link for #{pkg} is still present.
+            Please remove it manually: npm unlink -g #{pkg}
+          EOS
+        else
+          ohai "Removed npm link for #{pkg}."
+        end
+      end
+    end
+
+    npm_packages.each do |pkg|
+      if system "npm", "ls", "-g", "--depth=0", pkg
+        opoo <<~EOS
+          ATTENTION: Detected a global npm install for #{pkg}.
+          Attempting to uninstall to avoid conflicts with the Homebrew install.
+        EOS
+        if system "npm", "uninstall", "-g", pkg
+          ohai "Removed global npm install for #{pkg}."
+        else
+          odie <<~EOS
+            ATTENTION: Detected a global npm install of #{pkg} that could not be removed.
+            Please run: npm uninstall -g #{pkg}
+          EOS
+        end
+      end
+    end
+
     system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
